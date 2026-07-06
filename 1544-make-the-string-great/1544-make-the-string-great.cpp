0458c81e9b6e6b4e808421c959a8d5221cbb424b
Roshanambilwade <ambilwaderoshan@gmail.com>
2026-07-06 23:28:33 +0530

    Time: 0 ms (100%), Space: 8.5 MB (68.55%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string ans;
+        for(int i=0;i<s.length();i++){
+           if(!ans.empty() && abs(s[i]-ans.back())==32){
+            ans.pop_back();
+           }
+           else{
+            ans.push_back(s[i]);
+           }
+        }
+        return ans;
+    }
+};
