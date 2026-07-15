3e04182562e575183920b7c918d86dccd41d1b06
Roshanambilwade <ambilwaderoshan@gmail.com>
2026-07-15 10:46:35 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int oddsum = n*n;
-        int evensum = n*(n+1);
+        // int oddsum = n*n;
+        // int evensum = n*(n+1);
 
-        return gcd(oddsum,evensum);
+        // return gcd(oddsum,evensum);
+
+        // formula to calculate sum of first n odd numbers n = n*n;
+        // formula to calculate sum of first n even numbers n = n*(n+1);
+
+        int oddsum = n*n;
+         int evensum = n*(n+1);
+
+        // now find gcd using euledian algorithm
+        int a = oddsum;
+        int b = evensum;
+        while(b!=0){
+            int rem = a%b;
+            a = b;
+            b = rem;
+        }
+        return a;
+
     }
 };
